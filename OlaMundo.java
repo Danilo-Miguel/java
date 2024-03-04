7a480079886a5256ce4390f09594265d106ed0c5
Danilo-Miguel <dan.sm.mineiro@gmail.com>
2024-03-03 21:48:26 -0300

    JOptionPane teste

---
+++ OlaMundo.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane ;
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+        String nome;
+
+        nome = JOptionPane.showInputDialog("Escreva seu nome " );
+    }
+}
